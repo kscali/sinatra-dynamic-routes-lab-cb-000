33ccb3a31a0a78228c103967e485ab6c55f46f4e
Koy Saeteurn <kosatun@aol.com>
2018-09-30 05:24:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end 
   
   get '/square/:number' do 
-    @number = params{:number}.to_i * params{number}.to_i
+    @number = params{:number}.to_i * params{:number}.to_i
     "#{@number}"
   end   
 end
